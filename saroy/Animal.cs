41dc8cf260ec899b5121ed9fa146798ec9c54f1d
Sarah Roy <sarah2000roy@gmail.com>
2023-07-09 17:26:52 -0400

    Uploading Assignment 2 - ArrayList and Doubly Linked List

---
+++ saroy/Animal.cs
@@ -0,0 +1,79 @@
+﻿//COIS 2020H : Data Structure and Algorithms
+//Assigment 2
+//Name: Sarah Ann Roy
+//Student Number: 0650615
+//Animal.cs
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    public class Animal : IComparable<Animal>
+    {
+        //Animal Properties
+        public int ID { get; set; }
+        public string name { get; set; }
+        public double age { get; set; }
+        public string animal_type { get; set; } 
+        public Position pos;
+        private Random random = new Random();
+
+        //Animal Constructor - takes 3 parameters and assigns ID, name, age and position for each object.
+        public Animal(string t, int id, string n, double a)
+        {
+            this.ID = id; //assign ID
+            this.animal_type = t; //assign animal_type
+            this.name = n; //assign name
+            this.age = a; //assign age
+            pos = new Position(this.animal_type); //new position for animal
+        }
+        //Method: Move() creates 3 random values and passes the values to Position.Move() 
+        //Move all of the objects with a random dx, dy, dz (but only slightly, these can be +/- 5.0
+        public void Move()
+        {
+            double dx ,dy, dz;
+            if(this.animal_type == "Bird") //for Birds only
+            {
+                dx = random.Next(-10, 10);
+                dy = random.Next(-10, 10);
+                dz = random.Next(-2, 2);
+                
+            }
+            else //for Cats & Snakes
+            {
+                dx = random.Next(-5, 5);
+                dy = random.Next(-5, 5);
+                dz = random.Next(-5, 5);
+            }
+            this.pos.Move(this.animal_type,dx, dy, dz); //passing the random values to Position.Move()
+        }
+        
+        //Method: ToString() is virtual and is overriden in its subclasses
+        public virtual string ToString()
+        {
+            return "";
+        }
+
+        //Method: Eat() is virtual and is overriden in its subclasses
+        public virtual void Eat(Animal Bird)
+        {
+            
+        }
+
+        //Method: CompareTo() takes an Animal object and compares the names alphabetically 
+        /*The CompareTo() method depending on the comparison:
+         * returns 0, if the current instance’s property is equal to the temporary variable’s property (same name)
+         * returns 1, if the current instance’s property is greater than the temporary variable’s property
+         * returns -1, if the current instance’s property is less than the temporary variable’s property 
+         */
+        public int CompareTo(Animal animal)
+        {
+            if(animal == null)
+                return 1;
+            return this.name.CompareTo(animal.name);
+        }
+    }
+}
---
+++ saroy/Program.cs
@@ -0,0 +1,97 @@
+﻿//COIS 2020H : Data Structure and Algorithms
+//Assigment 2
+//Name: Sarah Ann Roy
+//Student Number: 0650615
+//Program.cs
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Assignment_1;
+
+//creating an ArrayList of 3 cats
+ArrayList<Animal> cats = new ArrayList<Animal>(3);
+
+//creating an ArrayList of 3 snakes
+ArrayList<Animal> snakes = new ArrayList<Animal>(3);
+
+//creating 3 objects of class Cat and adding to the arraylist 
+Animal bella = new Cat("Cat",1,"Bella",6.2, 0); cats.AddFront(bella);
+Animal charlie = new Cat("Cat", 2, "Charlie", 1.5, 1); cats.AddFront(charlie);
+Animal lucy = new Cat("Cat", 3, "Lucy", 4.3, 2); cats.AddFront(lucy);
+
+Console.WriteLine($"Number of cats = {cats.GetCount()}");
+
+//creating 3 objects of class Snake and adding to the arraylist 
+Animal coral = new Snake("Snake", 4, "Coral", 5.3, 24.45, true); snakes.AddFront(coral);
+Animal ebony = new Snake("Snake", 5, "Ebony", 0.4, 3.30, false); snakes.AddFront(ebony);
+Animal jade = new Snake("Snake", 6, "Jade", 9.6, 152.66, true); snakes.AddFront(jade);
+
+Console.WriteLine($"Number of snakes = {snakes.GetCount()}");
+
+//Merging the list of cats and snakes
+ArrayList<Animal> animals = ArrayList<Animal>.Merge(cats, snakes);
+
+//Testing PrintAllForward() on the merged array list
+Console.WriteLine("\nTesting PrintAllForward() for merged arrayList");
+Console.WriteLine(animals.PrintAllForward());
+
+//Testing PrintAllReverse() on the merged array list
+Console.WriteLine("\nTesting PrintAllReverse() for merged arrayList");
+Console.WriteLine(animals.PrintAllReverse());
+
+//Creating the first doubly linked list of birds
+DoublyLinkedList<Bird> birds1 = new DoublyLinkedList<Bird>();
+
+//creating 5 objects of class Bird and adding them to the first doubly linked list
+Animal tweety = new Bird("Bird", 1, "Tweety", 4.6);birds1.AddLast(tweety);
+Animal zazu = new Bird("Bird", 2, "Zazu", 5.2);birds1.AddLast(zazu);
+Animal iago = new Bird("Bird", 3, "Iago", 0.8);birds1.AddLast(iago);
+Animal hula = new Bird("Bird", 4, "Hula", 3.9);birds1.AddLast(hula);
+Animal manu = new Bird("Bird", 5, "Manu", 12.0);birds1.AddLast(manu);
+
+//Creating a second doubly linked list of birds
+DoublyLinkedList<Bird> birds2 = new DoublyLinkedList<Bird>();
+
+//creating 5 objects of class Bird and adding them to the second doubly linked list
+Animal couscous = new Bird("Bird", 6, "Couscous", 1.6);birds2.AddLast(couscous);
+Animal roo = new Bird("Bird", 7, "Roo", 4.6);birds2.AddLast(roo);
+Animal tookie = new Bird("Bird", 8, "Tookie", 5.2);birds2.AddLast(tookie);
+Animal plucky = new Bird("Bird", 9, "Plucky", 0.8);birds2.AddLast(plucky);
+Animal jay = new Bird("Bird", 10, "Jay", 1.6);birds2.AddLast(jay);
+
+//Merge the second list onto the first
+birds1.Merge(birds2);
+
+//Testing PrintAllForward() on the merged doubly linked list
+Console.WriteLine("\nTesting PrintAllForward() on merged Doubly Linked List");
+Console.WriteLine(birds1.PrintAllForward());
+
+//Testing PrintAllReverse() on the merged doubly linked list
+Console.WriteLine("\nTesting PrintAllReverse() on merged Doubly Linked List");
+Console.WriteLine(birds1.PrintAllReverse());
+
+int r = 0;//number of rounds it takes to eat all birds
+int range_c = 8, range_s = 3, speed_c = 16, speed_s = 14; //range and speeds of cats and snakes
+Animal toeat;
+//while-loop for the cats and snakes to eat all the birds
+while (birds1.GetCount()>0)
+{
+    for(int i = 0; i < animals.array.Length; i++)
+    {
+        toeat = birds1.FindClosest(animals.array[i]);//find closest bird to the animal
+        Console.WriteLine($"{animals.array[i].name} is Eating -> {toeat.name}");
+        animals.array[i].Eat(toeat);
+    }
+    
+    if (r % 5 == 0)//After every fifth iteration, PrintallForward 
+    {
+        Console.WriteLine($"After {r} rounds, the surviving birds are: \n");
+        birds1.PrintAllForward();
+    }
+    birds1.MoveBirds();//Then birds “that survive” will randomly move as per their rules above.  
+    r++;//update round count
+}
+Console.WriteLine($"Number of rounds it took to eat all birds = {r}");//Report how many rounds it took
