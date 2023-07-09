8a1d5d2e165b9458c787408f239b2dac2c4fbd3a
Sarah Roy <sarah2000roy@gmail.com>
2023-07-09 17:33:29 -0400

    Lab 6 Upload

---
+++ L6/DemoClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MinHeap
+{
+    class DemoClass : IComparable
+    {
+        public string name;
+        public int priority;
+     
+        public DemoClass()
+        { name = "";
+            priority = 0;
+        }
+
+        public int CompareTo(Object obj)
+        {   //CompareTo  
+            //this.CompareTo(value) returns < 0 if this < value
+            //this.CompareTo(value) returns >0 if this > value
+            DemoClass sample = (DemoClass)obj;
+
+            if (priority < sample.priority) return -1;
+            else if (priority > sample.priority) return 1;
+            else return 0; // value == sample.value
+
+        
+        }
+        public override string ToString()
+        {
+            return name + " " + priority + " ";
+        }
+
+    }
+}
---
+++ L6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MinHeap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Initialise a heap to some size
+            int sizeOfHeap = 10;
+            BinaryHeap<DemoClass> sampleHeap;
+            sampleHeap = new BinaryHeap<DemoClass>(sizeOfHeap);
+            Random rand = new Random();
+            //populate a heap
+            for (int i = 0; i < sizeOfHeap; i++)
+            {
+                DemoClass A = new DemoClass();
+                A.name = "Object # " + i;
+                A.priority = rand.Next(0, 100);
+                sampleHeap.AddItem(A);
+            }
+
+            //Print each  element in the heap
+
+            foreach (DemoClass i in sampleHeap)
+            {
+                Console.WriteLine(i.ToString() + " ");
+            }
+            
+            /* Ignore the following for now
+             * Console.WriteLine("\n Next we pop off the top element, a couple of times to illustrate what's happening");
+             * Console.WriteLine(sampleHeap.ExtractHead().ToString());
+             * Console.WriteLine(sampleHeap.ExtractHead().ToString());
+             * Console.WriteLine("The remaining Heap after two elements were removed");
+             * foreach (DemoClass i in sampleHeap)
+             * {
+             * if (i!=null)
+             * Console.WriteLine(i.ToString() + " ");
+             * }
+             * Console.ReadKey(); // only necessary if your project autocloses
+             * */
+        }
+    }
+}
